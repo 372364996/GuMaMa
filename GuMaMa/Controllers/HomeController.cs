a31a7230fe67423e7b6c31cccefa4314c8b4538e
杜鹏 <dupeng.tommy@outlook.com>
2018-05-09 17:31:22 +0800

    +将网站首页临时修改为‘博弈’demo

--- GuMaMa/Controllers/HomeController.cs
+++ GuMaMa/Controllers/HomeController.cs
@@ -26,5 +26,39 @@ namespace GuMaMa.Controllers
 
             return View();
         }
+
+        /// <summary>
+        /// 判断结果
+        /// </summary>
+        /// <param name="total">总分</param>
+        /// <param name="id">1，用户选择了正 2，用户选择了负</param>
+        /// <param name="clickCount">点击次数</param>
+        /// <param name="number">概率</param>
+        /// <returns></returns>
+        public JsonResult Result(int total, int id,int clickCount, double number = 36.7)
+        {
+            
+            clickCount += 1;
+            Random rd = new Random();
+            int rdNumber = rd.Next(0, 99);
+            if (rdNumber <= number && id == 1)
+            {
+
+                total += 3;
+                return Json(new { success = true, msg = "同正加3分", resultStr = "正", total, clickCount });
+            }
+            else if (rdNumber > number && id == 2)
+            {
+                total += 1;
+                return Json(new { success = true, msg = "同负加1分", resultStr = "负", total, clickCount });
+            }
+            else if (rdNumber < number && id == 2)
+            {
+                total -= 2;
+                return Json(new { success = true, msg = "正负相反减2分", resultStr = "正", total, clickCount });
+            }
+            total -= 2;
+            return Json(new { success = true, msg = "正负相反减2分", resultStr = "负", total, clickCount });
+        }
     }
 }
